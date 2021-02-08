09b2b76f25f5facfe0cb5c813c4f7d745983691f
Andrew Tiller <andrew.tiller@mymail.champlain.edu>
2021-02-07 20:00:28 -0500

    Slight rearranging on the server side no big changes

--- GPRO Net SDK/source/gpro-net-Console-Server/main-server.cpp
+++ GPRO Net SDK/source/gpro-net-Console-Server/main-server.cpp
@@ -24,6 +24,11 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <vector>
+#include <future>
+#include <limits>
+#include <iostream>
+#include <list>
 
 //RakNet
 #include <RakNet/RakPeerInterface.h>
@@ -38,77 +43,109 @@ enum GameMessages
 	ID_GAME_MESSAGE_1 = ID_USER_PACKET_ENUM + 1
 };
 
+struct ServerState 
+{
+	// not much need for anything else rn
+	RakNet::RakPeerInterface* peer;
+	//std::vector<RakNet::BitStream> serverMessages; 
+};
+
+void handleInput(ServerState* ss) 
+{
+	RakNet::Packet* packet;
+	for (packet = ss->peer->Receive(); packet; ss->peer->DeallocatePacket(packet), packet = ss->peer->Receive())
+	{
+		RakNet::MessageID msg;
+		RakNet::BitStream bsIn(packet->data, packet->length, false);
+		bsIn.Read(msg);
+
+
+		RakNet::Time timestamp = RakNet::GetTime(); //as a safe backup, just in case its not set
+		if (msg == ID_TIMESTAMP)
+		{
+			//todo handle time
+			bsIn.Read(timestamp);
+			bsIn.Read(msg);//now we update to show the real message
+		}
+
+		switch (msg)
+		{
+		case ID_REMOTE_DISCONNECTION_NOTIFICATION:
+			printf("Another client has disconnected.\n");
+			break;
+		case ID_REMOTE_CONNECTION_LOST:
+			printf("Another client has lost the connection.\n");
+			break;
+		case ID_REMOTE_NEW_INCOMING_CONNECTION:
+			printf("A client has connected.\n");
+
+			break;
+		case ID_NEW_INCOMING_CONNECTION:
+			printf("A connection is incoming.\n");
+			break;
+		case ID_NO_FREE_INCOMING_CONNECTIONS:
+			printf("The server is full.\n");
+			break;
+		case ID_DISCONNECTION_NOTIFICATION:
+			printf("A client has disconnected.\n");
+			break;
+		case ID_CONNECTION_LOST:
+			printf("A client lost the connection.\n");
+			break;
+		case ID_GAME_MESSAGE_1:
+		{
+			RakNet::BitStream untamperedBS(packet->data, packet->length, false);
+			ss->peer->Send(&untamperedBS, HIGH_PRIORITY, RELIABLE_ORDERED, 0, RakNet::UNASSIGNED_SYSTEM_ADDRESS, false);
+		}
+		break;
+		default:
+			printf("Message with identifier %i has arrived.\n", packet->data[0]);
+			break;
+		}
+	}
+}
+
+void handleUpdate(ServerState* ss)
+{
+	
+}
+
+void handleOutput(ServerState* ss)
+{
+	
+}
+
+
+
 int main(void)
 {
 	const unsigned short SERVER_PORT = 7777;
 	const unsigned short MAX_CLIENTS = 10;
 
+	ServerState ss[1] = { 0 };
 
-	RakNet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
+	ss->peer = RakNet::RakPeerInterface::GetInstance();
 	RakNet::Packet* packet;
 	RakNet::SocketDescriptor sd(SERVER_PORT, 0);
-	peer->Startup(MAX_CLIENTS, &sd, 1);
-	peer->SetMaximumIncomingConnections(MAX_CLIENTS);
+	ss->peer->Startup(MAX_CLIENTS, &sd, 1);
+	ss->peer->SetMaximumIncomingConnections(MAX_CLIENTS);
 	printf("Starting the server.\n");
 	// We need to let the server accept incoming connections from the clients
 
 
 	while (1)
 	{
-		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
-		{
-			RakNet::MessageID msg;
-			RakNet::BitStream bsIn(packet->data, packet->length, false);
-			bsIn.Read(msg);
+		
+		handleInput(ss);
 
+		//I dont think these are needed for the current assignment but could be useful for the future
+		//handleUpdate(ss);
 
-			RakNet::Time timestamp = RakNet::GetTime(); //as a safe backup, just in case its not set
-			if (msg == ID_TIMESTAMP)
-			{
-				//todo handle time
-				bsIn.Read(timestamp);
-				bsIn.Read(msg);//now we update to show the real message
-			}
-
-			switch (msg)
-			{
-			case ID_REMOTE_DISCONNECTION_NOTIFICATION:
-				printf("Another client has disconnected.\n");
-				break;
-			case ID_REMOTE_CONNECTION_LOST:
-				printf("Another client has lost the connection.\n");
-				break;
-			case ID_REMOTE_NEW_INCOMING_CONNECTION:
-				printf("A client has connected.\n");
-
-				break;
-			case ID_NEW_INCOMING_CONNECTION:
-				printf("A connection is incoming.\n");
-				break;
-			case ID_NO_FREE_INCOMING_CONNECTIONS:
-				printf("The server is full.\n");
-				break;
-			case ID_DISCONNECTION_NOTIFICATION:
-				printf("A client has disconnected.\n");
-				break;
-			case ID_CONNECTION_LOST:
-				printf("A client lost the connection.\n");
-				break;
-			case ID_GAME_MESSAGE_1:
-			{
-				RakNet::BitStream untamperedBS(packet->data, packet->length, false);
-				peer->Send(&untamperedBS, HIGH_PRIORITY, RELIABLE_ORDERED, 0, RakNet::UNASSIGNED_SYSTEM_ADDRESS, false);
-			}
-			break;
-			default:
-				printf("Message with identifier %i has arrived.\n", packet->data[0]);
-				break;
-			}
-		}
+		//handleOutput(ss);
 	}
 
 
-	RakNet::RakPeerInterface::DestroyInstance(peer);
+	RakNet::RakPeerInterface::DestroyInstance(ss->peer);
 
 	return 0;
 }
